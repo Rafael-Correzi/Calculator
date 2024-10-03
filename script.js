c4e3de03aea954de3f71f34d30ecbc5119506e9e
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2024-10-03 01:11:58 -0300

    Add basic skeleton of function operate

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ let operandA;
 let operandB;
 
 let operator;
+
+function operate(operandA, operandB, operator){
+  if (operator === "+") {
+    add(operandA, operandB);
+  }
+  else if (operator === "-") {
+    subtract(operandA, operandB);
+  }
+
+  else if (operator === "*") {
+    multiply(operandA, operandB);
+  }
+  
+  else if (operator === "/") {
+   divide(operandA, operandB);
+  }
+  
+  else return null;
+}
